1b3d99d0513a1f40471f77386f5b10f32d91112e
Kevin DeLand <Kevin DeLand>
2017-06-28 13:26:40 -0400

    added side robot div

--- js/NewDivInserter.js
+++ js/NewDivInserter.js
@@ -5,5 +5,12 @@ function NewDivInserter () {
   var $div = $("<div>", {id: "jarvis", "class": "robot"});
   $("body").append($div);
 
-  $div.load("jarvis.html");
+  var jarvisURL = chrome.runtime.getURL("../html/jarvis.html");
+  log(jarvisURL);
+  $div.load(jarvisURL, function() {
+    var robotContainer = $(".robotContainer")
+    log(robotContainer);
+    robotContainer.html('<img src="' + chrome.runtime.getURL("../images/bot.png") + '"/>' );
+  });
+
 };
