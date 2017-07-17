394853dcef8fd6c67f65ca6faba22117c52f7d29
Kevin DeLand <kevin.deland@gmail.com>
2017-07-17 11:06:49 -0400

    BUG: only some dates work on calendar

--- js/BotEventListener.js
+++ js/BotEventListener.js
@@ -105,12 +105,19 @@ function BotEventListener() {
   /*** Setting the goal end date *()*/
   var goalEndDate = $("#ctl00_cp_Content_tb_Target");
 
-  goalEndDate.on('mouseover', function() {
-    log('mouseover goalEndDate');
-  });
-
   goalEndDate.on('change', function() {
     log('change goalEndDate');
+  });
+
+  // DOES NOT WORK!
+  // TODO on click the calendar, check to see if GoalEndDate is valid, and is different
+  goalEndDate.on('blur', function() {
+    log('blur goalEndDate');
+
+    wizardState.goalEndDate = Date.parse(goalEndDate[0].value)
+    var diff = compareTestDates(wizardState.startTest.date, wizardState.goalEndDate);
+
+    showInterventionLengthBuddy(diff);
   });
 
   // some divs do not appear until this button is clicked, so we
@@ -119,24 +126,69 @@ function BotEventListener() {
   datePickerTrigger.on('click', function() {
     log('clicked datePickerTrigger');
 
-    var dateClickables = $(".ui-datepicker-calendar > tbody > tr > td").not(".ui-datepicker-unselectable");
-    log(dateClickables);
-    dateClickables.on('click', function(d) {
-        log('clicked date');
-        // log(d.currentTarget.outerText);
-        // log(d.currentTarget.textContent);
-        // log(d.currentTarget.outerHTML);
-        log(goalEndDate[0]);
-        log(goalEndDate[0].value);
-        wizardState.goalEndDate = Date.parse(goalEndDate[0].value)
-        var diff = compareTestDates(wizardState.startTest.date, wizardState.goalEndDate);
-
-        showInterventionLengthBuddy(diff);
-
-    });
+    addEventToCalendarPopup();
   });
 
+/*
+  var calendarPopupDiv = $("#ui-datepicker-div");
+  calendarPopupDiv.on('hide', function() {
+    log("closed it");
+  });
+  calendarPopupDiv.on('hide', function() {
+    log("closed it");
+  });
+  */
+
+  function addEventToCalendarPopup() {
+    log("addEventToCalendarPopup");
+
+    function addEventToCalendarDates() {
+      var calendar = $(".ui-datepicker-calendar");
+      calendar.on('change', function(d) {
+        log("CHANGED calendar");
+      });
+
+
+      var dateClickables = $(".ui-datepicker-calendar > tbody > tr > td").not(".ui-datepicker-unselectable");
+      var breakPoint = 5;
+      log(dateClickables);
+      dateClickables.on('hide', function(d) {
+        log("CHANGED CHANGED");
+      });
+      dateClickables.on('click', function(d) {
+          log('clicked date');
+          // log(d.currentTarget.outerText);
+          // log(d.currentTarget.textContent);
+          // log(d.currentTarget.outerHTML);
+          //log(goalEndDate[0]);
+          //log(goalEndDate[0].value);
+          // wizardState.goalEndDate = Date.parse(goalEndDate[0].value)
+          // var diff = compareTestDates(wizardState.startTest.date, wizardState.goalEndDate);
+          //
+          // showInterventionLengthBuddy(diff);
+
+      });
+    }
+
+    addEventToCalendarDates();
+
+    // have to re-add events
+    var changeMonthE = $(".ui-icon-circle-triangle-e");
+    var changeMonthW = $(".ui-icon-circle-triangle-w");
+    changeMonthE.on('click', function() {
+      log("clicked East");
+      addEventToCalendarPopup();
+    });
+    changeMonthW.on('click', function() {
+      log("clicked West");
+      addEventToCalendarPopup();
+    });
+
+  }
+
+
   function showInterventionLengthBuddy(diff) {
+    // TODO move if statement to message
     if(diff.valid) {
       botBuddy = {
         message : MESSAGES.interventionLengthLong.formatUnicorn({weeks: intToText(diff.weeks)}),
@@ -155,6 +207,9 @@ function BotEventListener() {
         buttonThree: {
           text: 'change date',
           callback: function() {
+            // TODO add click
+            datePickerTrigger.trigger("click");
+            addEventToCalendarPopup();
             hideBotBuddy();
           }
         }
@@ -178,6 +233,9 @@ function BotEventListener() {
         buttonThree: {
           text: 'change date',
           callback: function() {
+            // TODO add click
+            datePickerTrigger.trigger("click");
+            addEventToCalendarPopup();
             hideBotBuddy();
           }
         }
