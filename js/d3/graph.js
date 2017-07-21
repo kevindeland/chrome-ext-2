960048dbdfc42f49677fb6b044e6846641636e14
Kevin DeLand <kevin.deland@gmail.com>
2017-07-21 18:59:58 -0400

    added y axis to d3 chart

--- js/d3/graph.js
+++ js/d3/graph.js
@@ -9,7 +9,7 @@ function initializeD3() {
     return;
   }
 
-  var svgContainer = d3.select("#graph-body").append("svg")
+  var svg = d3.select("#graph-body").append("svg")
       .attr("width", svgWidth)
       .attr("height", svgHeight);
 
@@ -17,26 +17,28 @@ function initializeD3() {
 
 function redrawAxes() {
 
-    var containerWidth = $(".modalTitle").width();
-    var svgWidth = containerWidth,
-        svgHeight = 350;
-
     var margin = {
       top: 20,
       right: 20,
       bottom: 20,
-      left: 20
+      left: 60
     };
+
+    var containerWidth = $(".modalTitle").width() - margin.left - margin.right;
+    var svgWidth = containerWidth,
+        svgHeight = 350 - margin.top - margin.bottom;
+
 
     $("svg").empty();
 
-    var svgContainer = d3.select("svg");
-    svgContainer
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
+    var svg = d3.select("svg")
+        .attr("width", svgWidth + margin.left + margin.right)
+        .attr("height", svgHeight + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var width = svgWidth - margin.left - margin.right;
-    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth;// - margin.left - margin.right;
+    var height = svgHeight;// - margin.top - margin.bottom;
 
     var x = d3.time.scale()
         .range([0, width]);
@@ -49,24 +51,56 @@ function redrawAxes() {
     var xAxis = d3.svg.axis()
         .scale(x)
         .orient("bottom")
-        .ticks(5);
+        .ticks(5)
+        .tickFormat(function(d) {
+          // FIXME stack date and year
+          // https://stackoverflow.com/questions/13241475/how-do-i-include-newlines-in-labels-in-d3-charts
+          return d3.time.format("%b\n%y")(d);
+        });
 
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10);
+
+    // TODO make start date come from past historical data
     var startDate = myApp.data.getStartTest().date;
     log(startDate);
 
     var endDate = myApp.data.getEndDate();
 
     x.domain([startDate, endDate]);
+    y.domain([400, 600]) // FIXME dynamic
 
     // draw x axis
-    svgContainer.append("g")
+    svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0,"+ height + ")") // shift down by height
-        .call(xAxis)
+        .call(xAxis);
+        // IF we want a label
+        // .append("text")
+        // .attr("class", "label")
+        // .attr("x", width)
+        // .attr("y", -6)
+        // .style("text-anchor", "end")
+        // .text("Date");
+
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
         .append("text")
         .attr("class", "label")
-        .attr("x", width)
-        .attr("y", -6);
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Scaled Score (SS)");
+
+    // svg.append("g")
+    //     .attr("class", "y axis")
+    //     .call(yAxis)
+    //
 
 
 }
